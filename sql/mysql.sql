7dd470af9bfd16e9018428ea95c1ad9dcd373467
egawata <egawa.takashi@gmail.com>
2014-05-18 14:38:49 +0900

    add ddl/dml

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -1,4 +1,18 @@
-CREATE TABLE IF NOT EXISTS member (
-    id           INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name         VARCHAR(255)
+CREATE TABLE site (
+    id integer primary key,
+    name varchar(255) not null
 );
+
+CREATE TABLE zcr (
+    id integer primary key auto_increment,
+    site_id integer not null,
+    audio_id varchar(255) not null,
+    zcr numeric(10,5) not null,
+    unique key uniq_site_audio_id (site_id, audio_id)
+);
+
+INSERT INTO site (id, name) VALUES
+(1, 'YouTube'),
+(2, 'Terminal MP3 Download');
+
+
