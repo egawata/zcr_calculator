05d5cc0c77ecd354ecb548482a26ec126e8e506e
egawata <egawa.takashi@gmail.com>
2014-05-18 21:21:32 +0900

    add some columns to zcr

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -4,11 +4,15 @@ CREATE TABLE site (
 );
 
 CREATE TABLE zcr (
-    id integer primary key auto_increment,
-    site_id integer not null,
-    audio_id varchar(255) not null,
-    zcr numeric(10,5) not null,
-    unique key uniq_site_audio_id (site_id, audio_id)
+  id int(11) NOT NULL AUTO_INCREMENT,
+  site_id int(11) NOT NULL,
+  audio_id varchar(255) NOT NULL,
+  zcr decimal(10,5) NOT NULL,
+  title varchar(255) NOT NULL,
+  image_url varchar(255) DEFAULT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY uniq_site_audio_id (site_id, audio_id),
+  KEY idx_zcr (zcr)
 );
 
 INSERT INTO site (id, name) VALUES
